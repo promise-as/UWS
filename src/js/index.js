073843f5e9622d1f18c22c9b063490ab8c6a3ce9
Hong Baojin <13809735212@163.com>
2019-04-29 18:25:56 +0800

    write apply

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+  // 每一个num的高度取之对应的detail的高度
+  $(".honor .honor-row").each(function (index, ele) {
+    $(ele).children($("num")).outerHeight(
+        $($(".honor .detail")[index]).outerHeight(true)
+    );
+  });
+});
